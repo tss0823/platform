c5f6e5a0122cb9f29d1ed48964ab59cf96d33a37
shengshan.tang <shengshan.tang@git.yuntaohb.net>
2015-07-27 15:59:17 +0800

    test

--- src/main/java/com/usefullc/platform/common/utils/BeanUtils.java
+++ src/main/java/com/usefullc/platform/common/utils/BeanUtils.java
@@ -3,22 +3,17 @@
  */
 package com.usefullc.platform.common.utils;
 
-import java.beans.PropertyDescriptor;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
-
-import org.apache.commons.beanutils.PropertyUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.FatalBeanException;
-import org.springframework.util.Assert;
+import java.util.Set;
 
 /**
  * Bean 工具类
@@ -38,7 +33,7 @@ public class BeanUtils {
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
-	public static Map<String, Object> beanToQueryMap(Object bean) {
+	public static Map<String, Object> beanToMap(Object bean) {
 		try {
 			return PropertyUtils.describe(bean);
 		} catch (Exception e) {
@@ -66,6 +61,23 @@ public class BeanUtils {
 			throw new RuntimeException("bean copy error! ",e);
 		}
 	}
+	/**
+	 * bean 复制
+	 * @param srcBean bean 源对象
+	 * @param destBean 目标对象
+	 * @return
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void beanCopy(Object srcBean,Object destBean){
+		if(srcBean == null){
+			return;
+		}
+		try {
+			PropertyUtils.copyProperties(destBean, srcBean);
+		} catch (Exception e) {
+			throw new RuntimeException("bean copy error! ",e);
+		}
+	}
 
 	/**
 	 * 对象深度复制，条件各引用对象必须实现序列化
@@ -88,63 +100,20 @@ public class BeanUtils {
 		}
 		return destObj;
 	}
-	
-	/**
-	 * bean复制，对于NULL则不覆盖
-	 * @param source
-	 * @param target
-	 */
-	public static void beanCopy(Object source, Object target)  {
-		beanCopy(source, target, null, null);
-	}
-	
-	/**
-	 * bean复制，对于NULL则不覆盖
-	 * @param source
-	 * @param target
-	 * @param editable
-	 * @param ignoreProperties
-	 */
-	public static void beanCopy(Object source, Object target, Class<?> editable, String[] ignoreProperties){
-		Assert.notNull(source, "Source must not be null");
-		Assert.notNull(target, "Target must not be null");
 
-		Class<?> actualEditable = target.getClass();
-		if (editable != null) {
-			if (!editable.isInstance(target)) {
-				throw new IllegalArgumentException("Target class [" + target.getClass().getName() +
-						"] not assignable to Editable class [" + editable.getName() + "]");
-			}
-			actualEditable = editable;
-		}
-		PropertyDescriptor[] targetPds = org.springframework.beans.BeanUtils.getPropertyDescriptors(actualEditable);
-		List<String> ignoreList = (ignoreProperties != null) ? Arrays.asList(ignoreProperties) : null;
-
-		for (PropertyDescriptor targetPd : targetPds) {
-			if (targetPd.getWriteMethod() != null &&
-					(ignoreProperties == null || (!ignoreList.contains(targetPd.getName())))) {
-				PropertyDescriptor sourcePd = org.springframework.beans.BeanUtils.getPropertyDescriptor(source.getClass(), targetPd.getName());
-				if (sourcePd != null && sourcePd.getReadMethod() != null) {
-					try {
-						Method readMethod = sourcePd.getReadMethod();
-						if (!Modifier.isPublic(readMethod.getDeclaringClass().getModifiers())) {
-							readMethod.setAccessible(true);
-						}
-						Object value = readMethod.invoke(source);
-						Method writeMethod = targetPd.getWriteMethod();
-						if (!Modifier.isPublic(writeMethod.getDeclaringClass().getModifiers())) {
-							writeMethod.setAccessible(true);
-						}
-						if(value != null){
-							writeMethod.invoke(target, value);
-						}
-					}
-					catch (Throwable ex) {
-						throw new FatalBeanException("Could not copy properties from source to target", ex);
-					}
-				}
-			}
-		}
-	}
-	
+    public static Map<String, Object> beanToMapNotNull(Object srcBean) {
+        try {
+            Map<String, Object> map = PropertyUtils.describe(srcBean);
+            Set<String> key = new HashSet<String>(map.keySet());
+            for (String string : key) {
+                if (map.get(string) == null) {
+                    map.remove(string);
+                }
+            }
+            return map;
+        } catch (Exception e) {
+            log.error("bean to queryMap failed! ", e);
+            return null;
+        }
+    }
 }
